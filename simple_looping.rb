123b4ea816f17080bb39be5153531673de1564ed
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-02-26 16:54:23 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
   loop do 
     counter += 1
     puts "Welcome to Flatiron School's Web Development Course!"
-    if counter >= 10 
+    if counter >= 7 
       break 
     end
   # Code your solution here using the "loop" keyword to puts out the below phrase
